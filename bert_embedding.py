0e8952330981bb7f8c73e7ce01c9f3b48213002c
shaharmeir <107625064+shaharmeir@users.noreply.github.com>
2022-06-16 16:14:37 +0300

    Update bert_embedding.py

--- bert_embedding.py
+++ bert_embedding.py
@@ -2,11 +2,20 @@ from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 
 sentence_transformer_model = SentenceTransformer("all-distilroberta-v1")  # todo: also try ('all-MiniLM-L6-v2'), legal-bert
-
+sentence_transformer_model_mpnet = SentenceTransformer("all-mpnet-base-v2")
+sentence_transformer_model_legal_bert = SentenceTransformer("nlpaueb/legal-bert-base-uncased")
 
 def get_text_embedding(text: str):
     # todo: also try summarization before BERT
     return sentence_transformer_model.encode(text, show_progress_bar=True)
+
+def get_text_embedding_mpnet(text: str):
+    # todo: also try summarization before BERT
+    return sentence_transformer_model_mpnet.encode(text, show_progress_bar=True)
+
+def get_text_embedding_legal_bert(text: str):
+    # todo: also try summarization before BERT
+    return sentence_transformer_model_legal_bert.encode(text, show_progress_bar=True)
 
 # example_texts = ["today is gonna be a great day", "i dont feel so good today",
 #                  "you know it rains when there are such dark clouds in the sky",
