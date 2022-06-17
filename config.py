72dbef41831a4296f227386041ad60e1791aa325
shaharmeir <shahar1meir1@gmail.com>
2022-06-17 17:49:53 +0300

    My new commit

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ MAX_WORDS_TO_KEEP = 50
 
 MIN_REASON_COUNT_TO_KEEP = 30
 
-ORIGINAL_EXCEL_PATH = "/home/amit/Downloads/nlp_project_topic_modeling.xlsx"
-OUTPUT_WITH_EMBEDDING_PICKLE_PATH = "/home/amit/Downloads/nlp_project_topic_modeling.pkl"
+ORIGINAL_EXCEL_PATH = "/Users/shaharmeir/Downloads/paragraphs' classification.xlsx"
+OUTPUT_WITH_EMBEDDING_PICKLE_PATH = "/Users/shaharmeir/Downloads/nlp_project_topic_modeling.pkl"
